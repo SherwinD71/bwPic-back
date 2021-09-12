620230014e68cc76862bc81666916a5fe6d3b66f
ManuelBouzaCachaza <84750625+ManuelBouzaCachaza@users.noreply.github.com>
2021-09-12 18:39:50 +0200

    cambio en el initDB y listEntry

--- initDB.js
+++ initDB.js
@@ -24,7 +24,7 @@ async function main() {
       name VARCHAR(250) NOT NULL, 
       email VARCHAR(100) NOT NULL,
       userphoto VARCHAR(200),
-      password VARCHAR(20) NOT NULL,
+      password VARCHAR(250) NOT NULL,
       created_at DATETIME NOT NULL,
       active BOOLEAN DEFAULT false,
       role ENUM("admin","normal") DEFAULT "normal" NOT NULL,
@@ -41,7 +41,7 @@ async function main() {
     CREATE TABLE photos (
       id_photos INT NOT NULL AUTO_INCREMENT,
       url VARCHAR(200) NOT NULL,
-      description VARCHAR(500),
+      description TEXT,
       place VARCHAR(250),
       created_at DATETIME NOT NULL,
       PRIMARY KEY (id_photos),
@@ -75,32 +75,48 @@ async function main() {
       id_users INT NOT NULL,
       FOREIGN KEY (id_users) REFERENCES users(id_users))
     `);
+    console.log("Tablas creadas");
+
+    //añadir usuario admin
+    console.log("Añado usuario admin");
+    await connection.query(`
+    INSERT INTO users(created_at, email, password, name, username, active, role)
+      VALUES (
+        "${formatDateToDB(new Date())}", 
+        "admin@mail.com",
+        SHA2("${process.env.ADMIN_PASSWORD}", 512),
+        "admin", 
+        "admin", 
+        true, 
+        "admin");
+      `);
 
     //introducir usuarios faker
+    console.log("Añadir usuarios faker");
 
-    const numeroDeEntradas = 10;
+    const numeroUsers = 10;
 
-    for (let index = 0; index < numeroDeEntradas; index++) {
+    for (let index = 0; index < numeroUsers; index++) {
       let time = yourRandomGenerator(20, 8, 2);
       await connection.query(`
-  INSERT INTO users (username, name, email, userphoto, password, created_at, active, lastAuthUpdate )
+  INSERT INTO users (username, name, email, userphoto, password, created_at, active)
   VALUES (
            "${faker.internet.userName()}",
            "${faker.name.firstName()}",
            "${faker.internet.email()}",
            "${faker.image.avatar()}",
-           "${faker.internet.password()}",
-           "${formatDateToDB(new Date())}",
-           "${random(0, 1)}",
-           "${formatDateToDB(time)}"
-  )
+           SHA2("${faker.internet.password()}", 512),
+           "${formatDateToDB(time)}",
+           true
+             )
   `);
     }
 
     //   //introducir fotos faker
-    const fotosEntradas = 20;
+    console.log("Añadir fotos faker");
+    const numeroFotos = 20;
 
-    for (let index = 0; index < fotosEntradas; index++) {
+    for (let index = 0; index < numeroFotos; index++) {
       let time = yourRandomGenerator(20, 8, 2);
       await connection.query(`
      INSERT INTO photos (url, description, place, created_at, id_users)
@@ -110,11 +126,12 @@ async function main() {
               "${faker.lorem.paragraph()}",
               "${faker.address.city()}",
               "${formatDateToDB(time)}",
-              "${random(1, numeroDeEntradas)}"
+              "${random(2, numeroUsers + 1)}"
      )
      `);
     }
     // crear comentarios
+    console.log("Añadir comentario faker");
     const crearComentarios = 20;
     for (let index = 0; index < crearComentarios; index++) {
       let time = yourRandomGenerator(20, 8, 2);
@@ -124,8 +141,8 @@ async function main() {
 
             "${faker.lorem.words()}",
             "${formatDateToDB(time)}",
-            "${random(1, fotosEntradas)}",
-            "${random(1, numeroDeEntradas)}"
+            "${random(1, numeroFotos)}",
+            "${random(1, numeroUsers + 1)}"
 
     )
     `);
@@ -137,13 +154,13 @@ async function main() {
     for (let index = 0; index < crearVotos; index++) {
       let time = yourRandomGenerator(20, 8, 2);
       await connection.query(`
-      INSERT INTO likes (vote, created_at,id_photos, id_users)
+      INSERT INTO likes (vote, created_at, id_users, id_photos)
       VALUES (
 
-               "${random(0, 1)}",
+               "1",
                "${formatDateToDB(time)}",
-               "${random(1, fotosEntradas)}",
-               "${random(1, numeroDeEntradas)}"
+               "${random(2, numeroUsers + 1)}",
+               "${random(1, numeroFotos)}"
       )
       `);
     }
