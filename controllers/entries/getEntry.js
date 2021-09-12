23cd5808ce3fc9119ad32813d3142b95749add1c
ManuelBouzaCachaza <84750625+ManuelBouzaCachaza@users.noreply.github.com>
2021-09-12 19:41:34 +0200

    cambio en getEntry

--- controllers/entries/getEntry.js
+++ controllers/entries/getEntry.js
@@ -7,10 +7,22 @@ const getEntry = async (req, res, next) => {
 
     const { id } = req.params;
     const [result] = await connection.query(
-      `SELECT photos.url,users.username, photos.created_at, sum(likes.vote) as votos FROM photos 
-      join users on photos.id_users=users.id_users 
-      join likes on likes.id_photos=photos.id_photos
-      where photos.id_photos = ?`,
+      `SELECT p.id_photos, url, description, place, p.created_at, p.id_users, username, SUM(vote)
+      FROM photos AS p
+      LEFT JOIN likes ON (p.id_photos = likes.id_photos)
+      LEFT JOIN users ON (p.id_users = users.id_users)
+      WHERE p.id_photos = ?
+      GROUP BY p.id_photos;
+      `,
+
+      [id]
+    );
+    const [result2] = await connection.query(
+      `SELECT id_comments, comment_text, c.created_at, id_photos, c.id_users, username
+      FROM comments AS c
+      LEFT JOIN users ON (c.id_users = users.id_users)
+      WHERE id_photos = ?
+      `,
 
       [id]
     );
@@ -18,6 +30,7 @@ const getEntry = async (req, res, next) => {
     res.send({
       status: "ok",
       data: result,
+      data2: result2,
     });
   } catch (error) {
     next(error);
