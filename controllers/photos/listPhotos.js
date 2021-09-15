4a0582e20d7221f1166e577a145cf54e94dcbd47
SherwinD <sherwindavila@hotmail.com>
2021-09-15 20:44:12 +0200

    controller Listado Fotos

--- controllers/photos/listPhotos.js
+++ controllers/photos/listPhotos.js
@@ -6,21 +6,73 @@ const listPhotos = async (req, res, next) => {
     connection = await getDB();
     let result;
 
-    //FIXME añadir filtro y id usuario para ver solo las fotos de un usuario
+    // saco el query string
+    const { search, order, direction, user } = req.query;
 
-    [result] =
-      await connection.query(`SELECT p.id_photos, url, place, p.created_at, p.id_users, username, count(likes.id_photos) as likes, numComentarios
+    const validOrders = ["created_at", "likes", "numComentarios"];
+    const orderBy = validOrders.includes(order) ? order : "created_at";
+
+    const validDirections = ["ASC", "DESC"];
+    const orderDirection = validDirections.includes(direction)
+      ? direction
+      : "DESC";
+
+    // trozo de query comun
+    const queryListPhotos = `
+    SELECT p.id_photos, url, p.place, p.created_at, p.id_users, username, count(likes.id_photos) as likes, IFNULL(nComentarios, 0) as numComentarios
       FROM photos AS p
       LEFT JOIN likes ON (p.id_photos = likes.id_photos)
       LEFT JOIN users ON (p.id_users = users.id_users)
       LEFT JOIN (
-      SELECT id_photos, count(id_comments) AS numComentarios
+      SELECT id_photos, count(id_comments) AS nComentarios
       FROM comments
       GROUP BY id_photos
       ) AS c ON (p.id_photos = c.id_photos)
-      GROUP BY p.id_photos 
-      ORDER BY p.created_at desc;
-      `);
+    `;
+
+    if (search) {
+      if (user) {
+        [result] = await connection.query(
+          `
+        ${queryListPhotos}
+        WHERE (p.place LIKE ? OR p.description LIKE ?) AND p.id_users = ?
+        GROUP BY p.id_photos
+        ORDER BY ${orderBy} ${orderDirection}
+      `,
+          [`%${search}%`, `%${search}%`, user]
+        );
+      } else {
+        [result] = await connection.query(
+          `
+          ${queryListPhotos}
+          WHERE p.place LIKE ? OR p.description LIKE ? 
+          GROUP BY p.id_photos
+          ORDER BY ${orderBy} ${orderDirection}
+        `,
+          [`%${search}%`, `%${search}%`]
+        );
+      }
+    } else {
+      if (user) {
+        [result] = await connection.query(
+          `
+          ${queryListPhotos}
+          WHERE p.id_users = ?
+          GROUP BY p.id_photos
+          ORDER BY ${orderBy} ${orderDirection}
+        `,
+          user
+        );
+      } else {
+        [result] = await connection.query(
+          `
+          ${queryListPhotos}
+          GROUP BY p.id_photos
+          ORDER BY ${orderBy} ${orderDirection}
+        `
+        );
+      }
+    }
 
     res.send({
       status: "ok",
