a02efe3100f5745edbebdda9bf19b4fe9ee3a761
Stefano Peraldini <stefano.peraldini@gmail.com>
2021-09-28 15:46:27 +0200

    Modifico return endpoint

--- controllers/photos/commentPhoto.js
+++ controllers/photos/commentPhoto.js
@@ -5,6 +5,8 @@ const commentPhoto = async (req, res, next) => {
   try {
     connection = await getDB();
 
+    let result;
+
     const { id } = req.params;
 
     const { comment } = req.body;
@@ -20,12 +22,29 @@ const commentPhoto = async (req, res, next) => {
       [now, id, req.userAuth.id, comment]
     );
 
+    // devuelvo numero commentario
+    [result] = await connection.query(
+      `
+      SELECT id_photos, count(id_comments) AS nComentarios
+      FROM comments
+      WHERE id_photos=?
+    `,
+      [id]
+    );
+
+    const [result2] = await connection.query(
+      `SELECT id_comments, comment_text, c.created_at, id_photos, c.id_users, username, userphoto
+      FROM comments AS c
+      LEFT JOIN users ON (c.id_users = users.id_users)
+      WHERE id_photos = ?
+      ORDER BY c.created_at DESC
+      `,
+      [id]
+    );
+
     res.send({
       status: "ok",
-      data: {
-        status: "ok",
-        message: "Nuevo comentario creado.",
-      },
+      data: { ...result[0], comments: result2 },
     });
   } catch (error) {
     next(error);
--- controllers/photos/getPhoto.js
+++ controllers/photos/getPhoto.js
@@ -7,7 +7,7 @@ const getPhoto = async (req, res, next) => {
 
     const { id } = req.params;
     const [result] = await connection.query(
-      `SELECT p.id_photos, url, description, place, p.created_at, p.id_users, username, count(likes.id_photos) as likes
+      `SELECT p.id_photos, url, description, place, p.created_at, p.id_users, userphoto, username, count(likes.id_photos) as likes
       FROM photos AS p
       LEFT JOIN likes ON (p.id_photos = likes.id_photos)
       LEFT JOIN users ON (p.id_users = users.id_users)
@@ -18,11 +18,11 @@ const getPhoto = async (req, res, next) => {
       [id]
     );
     const [result2] = await connection.query(
-      `SELECT id_comments, comment_text, c.created_at, id_photos, c.id_users, username
+      `SELECT id_comments, comment_text, c.created_at, id_photos, c.id_users, username, userphoto
       FROM comments AS c
       LEFT JOIN users ON (c.id_users = users.id_users)
       WHERE id_photos = ?
-      ORDER BY c.created_at
+      ORDER BY c.created_at DESC
       `,
       [id]
     );
